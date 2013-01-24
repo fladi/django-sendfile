e6272e6969d64fb21f3845c802a0a291ba9a14d5
Michael Fladischer <FladischerMichael@fladi.at>
2013-01-24 12:37:08 +0100

    Detect conflict with pysendfile and change package name accordingly.

--- setup.py
+++ setup.py
@@ -1,8 +1,32 @@
 from distutils.core import setup
+import os
+
+# Try to import any previously install sendfile package.
+# If this succeeds, check if it looks the same as this package, so we don't
+# change the package name during an update.
+try:
+    import sendfile
+except ImportError:
+    package='sendfile'
+    imported=False
+else:
+    imported=True
+    if sendfile.__file__ != 'sendfile/__init__.py':
+        package='django_sendfile'
+    else:
+        package='sendfile'
+# Ensure sendfile is loaded from the 'src' path, so the version information
+# can be extracted.
+os.sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
+if imported:
+    reload(sendfile)
+else:
+    import sendfile
+
 
 setup(
     name='django-sendfile',
-    version=__import__('sendfile').__version__,
+    version=sendfile.__version__,
     description='Abstraction to offload file uploads to web-server (e.g. Apache with mod_xsendfile) once Django has checked permissions etc.',
     long_description=open('README.rst').read(),
     author='John Montgomery',
@@ -10,18 +34,18 @@ setup(
     url='http://github.com/johnsensible/django-sendfile',
     download_url='http://github.com/johnsensible/django-sendfile/downloads',
     license='BSD',
-    
+
     requires=['Django (>=1.4.2)'],
 
-    packages=['sendfile', 'sendfile.backends'],
+    packages=[package, '%s.backends' % package],
     package_dir={
-        'sendfile': 'sendfile',
-        'sendfile.backends': 'sendfile/backends',
+        package: 'src/sendfile',
+        '%s.backends' % package: 'src/sendfile/backends',
     },
     package_data = {
         'sendfile': ['testfile.txt'],
     },
-    
+
     zip_safe=True,
     classifiers=[
         'Development Status :: 4 - Beta',
